ea1feea2d6c66749dd0807b6331db219b2f498b6
Kenny Yu <kennyyu@fb.com>
2023-12-10 15:21:39 -0800

    Site published at Sun Dec 10 15:21:39 PST 2023

--- src/materials/classic/textures/index.ts
+++
@@ -1,8 +0,0 @@
-import blue from './blue.png';
-import green from './green.png';
-import orange from './orange.png';
-import red from './red.png';
-import white from './white.png';
-import yellow from './yellow.png';
-
-export { blue, green, orange, red, white, yellow };
